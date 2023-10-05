76c28039a7ba9b101e429770f91d2a2eab29fff5
HansAlej0 <hanscalderon.estudiante@iser.edu.co>
2023-10-05 07:15:31 -0500

    Learn Vue.js

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <card-container />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import CardContainer from "./components/CardContainer.vue";
+
+export default defineComponent({
+  components: {
+    CardContainer,
+  },
+});
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/CardContainer.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card-container">
+        <Card v-for="post in posts" :key="post.title" :title="post.title" :text="post.text" :link="post.link"/>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import datos from "@/assets/json/datos.json";
+import Card from "./Card.vue";
+
+export default defineComponent({
+    name: "CardContainer",
+    components: {
+        Card
+    },
+    computed: {
+        posts: () => {
+            return datos.map((post: any) => {
+                return post;
+            });
+        },
+    },
+});
+</script>
+
+<style scoped lang="scss">
+</style>
